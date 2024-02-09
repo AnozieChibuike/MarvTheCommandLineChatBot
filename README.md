2fe02a282a98021983a4d9d1f5121bce6cc46d49
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-02-09 11:14:46 +0100

    Marv the command Line chatbot

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Sarcastic Bot
+
+## Usage
+
+- Get your api key from openAI
+- Create a `.env` file in your root directory
+- Store your api key in the `.env` file in this format `OPENAI_API_KEY=<Your API Key>`
+- Run the application `python3 marv.py`
+- Optional, You can choose the bot name before run `python3 marv.py --name <your custom bot name>`
+- To stop the bot press `Ctrl + C` on windows and `Command + C` on MacOS
---
+++ marv.py
@@ -0,0 +1,57 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import json
+from colorama import Fore, Back, Style
+from unratedwriting import typewrite
+import argparse
+
+
+load_dotenv()
+client = OpenAI()
+
+
+def main(bot_name="Marv"):
+    print()
+    typewrite(f"Hi I am {Fore.YELLOW}{bot_name}{Style.RESET_ALL} your sarcastic AI bot")
+    print()
+    name = input(f"What is your name: {Fore.GREEN}")
+    print(f"{Style.RESET_ALL}")
+    typewrite("Enter your message")
+    print()
+    messages = [
+        {
+            "role": "system",
+            "content": f"You are {bot_name}, a chatbot that reluctantly answers questions with sarcastic responses.",
+        },
+    ]
+
+    while True:
+        inp = input(f"{Fore.GREEN}{name}{Style.RESET_ALL}: ")
+        messages.append({"role": "user", "content": inp})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        jsonResponse = json.loads(response.model_dump_json())["choices"][0]["message"][
+            "content"
+        ]
+        messages.append({"role": "assistant", "content": jsonResponse})
+        print()
+        typewrite(f"{Fore.YELLOW}{bot_name}{Style.RESET_ALL}: {jsonResponse}")
+        print()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Name the bot.")
+    parser.add_argument("--name", type=str, help="Specify the bot name")
+    args = parser.parse_args()
+    if args.name:
+        main(bot_name=args.name)
+    else:
+        main()
