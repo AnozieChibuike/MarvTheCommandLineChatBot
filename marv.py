cdada82cb70ed9b111b5bd4621af9baf249df797
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-02-09 20:34:00 +0100

    basic error fixes

--- marv.py
+++ marv.py
@@ -1,13 +1,20 @@
-from openai import OpenAI
+from openai import OpenAI, APIConnectionError, OpenAIError, AuthenticationError
 from dotenv import load_dotenv
 import json
 from colorama import Fore, Back, Style
 from unratedwriting import typewrite
 import argparse
+import os
 
 
 load_dotenv()
-client = OpenAI()
+try:
+    client = OpenAI()
+except OpenAIError as e:
+    print()
+    typewrite(f'{Fore.RED}RuntimeError: {e}{Style.RESET_ALL}')
+    print()
+    exit(1)
 
 
 def main(bot_name="Marv"):
@@ -24,27 +31,41 @@ def main(bot_name="Marv"):
             "content": f"You are {bot_name}, a chatbot that reluctantly answers questions with sarcastic responses.",
         },
     ]
-
+    retries = 0
     while True:
-        inp = input(f"{Fore.GREEN}{name}{Style.RESET_ALL}: ")
-        messages.append({"role": "user", "content": inp})
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=0.5,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
+            inp = input(f"{Fore.GREEN}{name}{Style.RESET_ALL}: ")
+            messages.append({"role": "user", "content": inp})
+            try:
+                response = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages,
+                    temperature=0.5,
+                    max_tokens=256,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                )
 
-        jsonResponse = json.loads(response.model_dump_json())["choices"][0]["message"][
-            "content"
-        ]
-        messages.append({"role": "assistant", "content": jsonResponse})
-        print()
-        typewrite(f"{Fore.YELLOW}{bot_name}{Style.RESET_ALL}: {jsonResponse}")
-        print()
+                jsonResponse = json.loads(response.model_dump_json())["choices"][0]["message"][
+                    "content"
+                ]
+                messages.append({"role": "assistant", "content": jsonResponse})
+                print()
+                typewrite(f"{Fore.YELLOW}{bot_name}{Style.RESET_ALL}: {jsonResponse}")
+                print()
+            except APIConnectionError:
+                if retries == 3:
+                    break
+                print()
+                typewrite(f'{Fore.RED}ConnectionError: You need an internet connection to use Marv{Style.RESET_ALL}')
+                print()
+                retries += 1
+                typewrite(f'retrying({retries}/3).....')
+                print()
+            except AuthenticationError:
+                typewrite(f'{Fore.RED}AuthenticationError: Invalid or Incorrect API_KEY{Style.RESET_ALL}')
+                break
+                
 
 
 if __name__ == "__main__":
